b5d1b0a016df92ddac434e3b77137787ef4eac6e
wojtekssm <96737714+wojtekssm@users.noreply.github.com>
2024-02-29 19:58:48 +0100

    Add files via upload

---
+++ 29.02.2024 BEZ KALENDARZA/main.php
@@ -0,0 +1,74 @@
+<html lang="pl">
+<body>
+
+
+
+<div class="main_container">
+    <div class="main_baner_box"></div>
+    <div class="content_container">
+        <div class="main_reference_container">
+        <div class="main_reference_box">
+
+            <a href="#">
+            <div class="main_reference">
+                    <img src="pics/icons/teacher_colored.png" alt="ikona" class="reference_icon">
+                    <p class="reference_text">Zastępstwa</p>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="main_reference">
+                <img src="pics/icons/gallery_colored.png" alt="ikona" class="reference_icon">
+                <p class="reference_text">Galeria</p>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="main_reference">
+                <img src="pics/icons/student_colored.png" alt="ikona" class="reference_icon">
+                <p class="reference_text">Rekrutacja</p>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="main_reference">
+                    <img src="pics/icons/online-learning_colored.png" alt="ikona" class="reference_icon">
+                    <p class="reference_text">E-dziennik</p>
+            </div>
+            </a>
+
+        </div>
+        </div>
+
+    <div class="announcments-container">
+        <div class="announcment-box" data-type="ogloszenie">
+            <div>
+                <h3>Próbne, ustne egzaminy maturalne z języka polskiego</h3>
+                <h3>2024-02-21</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eos maxime, ex ipsam nostrum dolorem perspiciatis? Earum id accusantium animi reprehenderit commodi, incidunt molestiae, libero nam aut corporis numquam error pariatur ipsa facere doloremque ut possimus exercitationem perspiciatis iste ea cupiditate. Dolore aliquam eveniet tenetur officia dolores error suscipit. Esse?</p>
+        </div>
+        <div class="announcment-box" data-type="konkurs">
+            <div>
+                <h3>Szkolny konkurs karaoke</h3>
+                <h3>2024-02-21</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, deleniti quaerat commodi quis est, doloremque corporis cum repudiandae velit minus iste beatae aut rem earum maiores eos. Autem corrupti nesciunt eius illo a id rem excepturi fugiat odit laboriosam, necessitatibus natus omnis adipisci cupiditate voluptates officiis ducimus laudantium consequuntur accusantium reprehenderit ex! Pariatur quasi reprehenderit laudantium saepe repellat optio quia minima ab provident soluta, in aperiam excepturi doloribus! Tenetur libero atque ratione incidunt, sit numquam ex dolorem minus sapiente! Ut.</p>
+        </div>
+        <div class="announcment-box" data-type="ogloszenie">
+            <div>
+                <h3>Tydzień Języków Obcych</h3>
+                <h3>2024-02-21</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ullam ab voluptate perferendis velit hic magnam nostrum sequi, natus officia soluta libero atque eos sint reiciendis. Excepturi esse officia ab illum aut.</p>
+        </div>
+        </div>
+
+        <a class="announcment_show_all_button" href="#">Pokaż wszystkie</a>
+    </div>
+</div>
+
+
+
+</body>
+</html>
