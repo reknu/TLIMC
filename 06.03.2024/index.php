e3bd7484e357d5951407f366a96a33dc85218bca
wojtekssm <96737714+wojtekssm@users.noreply.github.com>
2024-03-06 20:39:45 +0100

    Add files via upload

--- 06.03.2024/index.php
+++ 06.03.2024/index.php
@@ -1,1 +1,41 @@
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <title>TLiMC</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+    </head>
+<body>
 
+<?php
+    if (!isset($_GET["site"]))
+        {$file = 'main';}
+    else
+        {$file = $_GET["site"];}
+
+
+    $ext = '.php';
+
+    
+    echo '<div id="container">';
+        include("menu_bar.php");
+        include("$file$ext");
+        include("footer.php");
+    echo '</div>';
+
+?>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ 06.03.2024/menu_bar.php
@@ -0,0 +1,46 @@
+<html lang="pl">
+    
+<body>
+
+
+<div class="menu_bar_container">
+    <div class="menu_bar_box">
+        <div class="menu_bar_logotypes">
+            <a href="index.php">
+            <div class="menu_bar_logo_box">
+                <img src="pics/tlimc_logo.png" alt="logo tlimc" class="tlimc_logo">
+            </div>
+            </a>
+        </div>
+
+        <div class="menu_content_box">
+            <div class="menu_contact_box">
+
+                <div class="menu_contact_content"> 
+                    <img src="pics/icons/mail.png" alt="ikonka" class="menu_contact_content_icon">
+                    <a href="mailto:sekretariat@tlimc.szczecin.pl" title="Napisz - sekretariat@tlimc.szczecin.pl" class="menu_contact_text">sekretariat@tlimc.szczecin.pl</a>
+                </div>
+
+                <div class="menu_contact_content">
+                    <img src="pics/icons/phone.png" alt="ikonka" class="menu_contact_content_icon">
+                    <a href="tel:+48914848056" title="Zadzwoń - +48 91 48 48 056" class="menu_contact_text">+48 91 48 48 056</a>
+                </div>  
+            </div>
+            <div class="nav_separator">
+                <hr>
+            </div>
+            <div class="menu_buttons_box">
+                <a href="" class="menu_button">Plan lekcji</a>
+                <a href="" class="menu_button">Uczeń</a>
+                <a href="" class="menu_button">Rodzic</a>
+                <a href="" class="menu_button">Szkoła</a>
+                <a href="" class="menu_button">Kontakt</a>
+            </div>
+        </div>
+        <div class="menu_burger_box"></div>
+    </div>
+
+</div>
+
+</body>
+</html>
