b4b85b041eaa0edb451ecb3335baed27ab7b9fc1
reknu <130186345+reknu@users.noreply.github.com>
2024-04-18 18:24:31 +0200

    Delete 14.03.2024 directory

--- 14.03.2024/announcement_details.php
+++
@@ -1,9 +0,0 @@
-<div class="content_container">
-    <div class="announcement_full_box">
-    <h1 class="section_text">Nazwa ogłoszenia, długa nazwa coś Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, nobis.</h1>
-    <div class="announcement_details_image_and_text">
-        <img src="pics/aktualnosci/3.jpg" alt="">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum omnis magnam quisquam? Tenetur aut impedit amet quod doloremque sunt ipsam ad at odit. Fuga, quidem! Iste ab minus provident numquam, ut aut id architecto ipsam eos nulla nisi accusamus ipsum?</p>
-    </div>
-    </div>
-</div>
